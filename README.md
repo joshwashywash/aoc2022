b5c9180df7a92a03f0d33a22e7294725de217e99
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-12-07 20:45:18 -0800

    add help usage

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Make sure an input file is available at root and run:
 
 ```console
-dart run ./bin/aoc2022.dart --file day1.example.txt --day 1
+dart run ./bin/aoc2022.dart --input day1.example.txt --day 1
 ```
 
 or even
 
 ```console
-dart run ./bin/aoc2022.dart -f day1.example.txt -d 1
+dart run ./bin/aoc2022.dart -i day1.example.txt -d 1
 ```
--- bin/aoc2022.dart
+++ bin/aoc2022.dart
@@ -3,23 +3,29 @@ import 'package:args/args.dart';
 import 'package:aoc2022/aoc2022.dart';
 
 void main(List<String> arguments) async {
-  const days = 5;
+  const days = 1;
 
-  final allowed =
-      List<int>.generate(days, (index) => 1 + index, growable: false)
-          .map((e) => '$e');
+  // final allowed =
+  //     List<int>.generate(days, (index) => 1 + index, growable: false)
+  //         .map((e) => '$e');
+  final allowed = [for (var i = 0; i < days; i += 1) '${i + 1}'];
 
   final parser = ArgParser()
-    ..addOption('file', abbr: 'f')
-    ..addOption('day', abbr: 'd', allowed: allowed);
+    ..addOption('input', abbr: 'i', help: 'File path of the input file.')
+    ..addOption('day', abbr: 'd', allowed: allowed, help: 'Day number to use.')
+    ..addFlag('help', abbr: 'h', negatable: false);
 
   final results = parser.parse(arguments);
-  final file = File(results['file']);
-  final day = results['day'];
+  if (results['help']) {
+    print(parser.usage);
+  } else {
+    final file = File(results['input']);
+    final day = results['day'];
 
-  final map = {'1': day1};
-  if (map.containsKey(day)) {
-    final result = await map[day]!(file);
-    print(result);
+    final map = {'1': day1};
+    if (map.containsKey(day)) {
+      final result = await map[day]!(file);
+      print(result);
+    }
   }
 }
