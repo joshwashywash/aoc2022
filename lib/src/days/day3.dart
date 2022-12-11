e06ab19f257dd1b9ee2ddcce11d61ac984cd37be
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-12-10 18:50:29 -0800

    day 3 part 2 done

--- lib/src/days/day3.dart
+++ lib/src/days/day3.dart
@@ -1,4 +1,7 @@
 import 'dart:io';
+
+import 'package:collection/collection.dart';
+
 import '../utils.dart' as utils;
 
 final cases = [
@@ -20,16 +23,26 @@ final flipped = utils.reverseMap(alphabet.asMap());
 Future<List<int>> day3(File file) async {
   final content = await file.readAsLines();
 
-  final sum = content.map((line) {
+  final part1 = content.map((line) {
     final halfway = line.length ~/ 2;
-    final common = [
-      line.substring(0, halfway),
-      line.substring(halfway),
-    ]
-        .map((piece) => piece.split('').toSet())
+
+    final common = line
+        .split('')
+        .slices(halfway)
+        .map((piece) => piece.toSet())
+        .reduce((a, b) => a.intersection(b));
+
+    return (flipped[common.first] ?? 0) + 1;
+  }).reduce((a, b) => a + b);
+
+  const groupSize = 3;
+
+  final part2 = content.slices(groupSize).map((group) {
+    final common = group
+        .map((list) => list.split('').toSet())
         .reduce((a, b) => a.intersection(b));
     return (flipped[common.first] ?? 0) + 1;
   }).reduce((a, b) => a + b);
 
-  return [sum];
+  return [part1, part2];
 }
