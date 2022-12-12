37960abbbfb73cabc77b563638e965d32b6599c9
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-12-11 17:09:29 -0800

    add day 4 part 2

--- lib/src/days/day4.dart
+++ lib/src/days/day4.dart
@@ -7,15 +7,21 @@ bool fullyContains(Iterable<Iterable<num>> pairs) {
           pairs.last.last >= pairs.first.last);
 }
 
+bool overlaps(Iterable<Iterable<num>> pairs) {
+  return pairs.first.first <= pairs.last.last &&
+      pairs.first.last >= pairs.last.first;
+}
+
 Future<List<int>> day4(File file) async {
   final lines = await file.readAsLines();
 
   final pairs = lines.map(
       (line) => line.split(',').map((sep) => sep.split('-').map(int.parse)));
 
-  final part1 = pairs.map((pair) => fullyContains(pair));
+  final part1 = pairs.map(fullyContains);
+  final part2 = pairs.map(overlaps);
 
-  return [part1].map((p) {
+  return [part1, part2].map((p) {
     return p.map((predicate) => predicate ? 1 : 0).reduce((a, b) => a + b);
   }).toList();
 }
